d83797b32af73f0dea9b488071fbeae072e5e2e5
Adam Sisk <adamsisk91@gmail.com>
2019-08-05 10:55:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Adam is the champion'
+document.appendChild(newHeader)
